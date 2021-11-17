151ae7dc9a05a1494a1f831bca3ecede47e14a9f
namsi-lia <lydianamsi@gmail.com>
2021-11-17 14:34:29 +0300

    my first flutter commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/models/recipe.api.dart';
+import 'package:recipe_app/views/widgets/recipe_card.dart';
+import 'models/recipe.dart';
+
+void main() {
+  runApp(MaterialApp());
+  home:MyApp();
+}
+
+class MyApp extends StatefulWidget {
+
+
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late List<Recipe> _recipes;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getRecipe();
+  }
+
+  Future<void> getRecipe() async {
+    _recipes = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        appBar: AppBar(
+        title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Icon(Icons.restaurant_menu),
+    SizedBox(width: 10),
+    Text('Food Recipes'),
+    ],
+    ),
+    ),
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+          itemCount: _recipes.length,
+          itemBuilder: (context, index) {
+            return RecipeCard(
+                title: _recipes[index].name,
+                cookTime: _recipes[index].totalTime,
+                rating: _recipes[index].rating.toString(),
+                thumbnailUrl: _recipes[index].images);
+          },
+        ));
+
+  }
+}
---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:recipe_app/models/recipe.dart';
+import 'package:http/http.dart' as http;
+
+import 'recipe.dart';
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "a0f3c28503msh99b718d992f2885p1039dbjsn30fc653805f0",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List _temp = [];
+
+    for (var i in data['feed']) {
+      _temp.add(i['content']['details']);
+    }
+    return Recipe.recipesFromSnapshot(_temp);
+  }
+}
